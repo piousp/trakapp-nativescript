795c6254b58b7d572f701aab2c9b238b0cbfb088
Rodrigo Paris <rodrigo.paris@ciriscr.com>
2018-11-01 11:44:01 -0600

    Feat: Que las subtareas tengan rutas y poder ver las ubicaciones de cada subtarea

--- app/config/axios.js
+++ app/config/axios.js
@@ -5,25 +5,8 @@ import axios from "axios";
 console.log(process.env);
 
 const a = axios.create({
-  //baseUrl: process.env.API_URL || "http://trakapp.ciriscr.com:7001",
-  baseUrl: process.env.API_URL || "http://192.168.1.38:3001",
+  baseUrl: process.env.API_URL || "http://trakapp.ciriscr.com:7001",
 });
-
-/* a.interceptors.request.use((config) => {
-  console.log(Vue.prototype.$Progress.start());
-  return config;
-}, (error) => {
-  console.log(Vue.prototype.$Progress.fail());
-  return Promise.reject(error);
-});
-
-a.interceptors.response.use((response) => {
-  console.log(Vue.prototype.$Progress.finish());
-  return response;
-}, (error) => {
-  console.log(Vue.prototype.$Progress.fail());
-  return Promise.reject(error);
-}); */
 
 Vue.use(VueAxios, a);
 
